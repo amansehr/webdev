d0eb7df08a4e2102c5e3cd46671646963dc465b4
deadlyaman <amansehrawat218@gmail.com>
2020-09-24 14:16:33 +0530

    express

--- nodejs/express/to-do-list using express/server.js
+++ nodejs/express/to-do-list using express/server.js
@@ -34,10 +34,10 @@ app.get('/', (req, res) => {
 
 
 app.post('/', (req, res) => {
-    task.push(req.body.li)
-    res.redirect('/')
+    task.push(req.body.li) //! it will push to an array
+    res.redirect('/') //! this will redirect to root page
 })
-
+//! jabi bhi redirect use karta h toh http 302 code use hota {or koi ho sakta h check kar}and agar humara pass address h ki kha jana h huma .toh browser automatically get request karta h ush address per
 
 app.listen(4444, () => {
     console.log("started at port 4444")
